fecf5dc5eb75663310815e0b115b490efb49ebad
Kurt Cantuba <116888719+Sneakybooger@users.noreply.github.com>
2023-03-30 18:19:40 +0800

    Create Grade Calculator.java

---
+++ Conditions | Beecrowd answers/Grade Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      
+     System.out.println("Enter Grade in English: ");
+     double English = sc.nextDouble();
+     System.out.println("Enter Grade in Math: ");
+     double Math = sc.nextDouble();
+     System.out.println("Enter Grade in Science: ");
+     double Science = sc.nextDouble();
+     System.out.println("Enter Grade in Computer: ");
+     double Computer = sc.nextDouble();
+     double ave = (English+Math+Science+Computer)/4; 
+     System.out.println("");
+     System.out.println("Average: " + ave);
+     
+   if(ave >= 100) {    
+	   System.out.println("INVALID GRADE");
+   }
+   else if (ave >= 98) {  
+	   System.out.println("With Highest Honors");
+   }
+   else if (ave >= 95) {
+	   System.out.println("With High Honors");
+   }
+   else if(ave >= 90) {
+	   System.out.println("With Honors");
+   }
+   else if(ave >= 75) { 
+	   System.out.println("PASSED");
+   }
+   else {
+	   System.out.println("FAILED");
+   }
+   
+	}   
