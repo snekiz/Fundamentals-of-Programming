94c3eb45c0ec56305cb7d3c6efd1832f43c88fb3
Kurt Cantuba <116888719+Sneakybooger@users.noreply.github.com>
2023-03-30 18:50:37 +0800

    Create StringWithAsteris.java

---
+++ LOOPING/Beecrowd/StringWithAsteris.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class StringWithAsteris {
+
+	public static void main(String[] args) {
+//      Write a program that will read a string and print the string with * after each character
+     Scanner sc = new Scanner(System.in);
+     
+     String s = sc.nextLine();
+     for(int i = s.length(); i>=0; i++) {
+    	   System.out.println(i);
+     }
+
+	}
+
+}
