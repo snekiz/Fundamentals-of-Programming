fd24b07cd1977488082ade2302f0af1ed939f2ee
Kurt Cantuba <116888719+Sneakybooger@users.noreply.github.com>
2023-03-30 19:07:23 +0800

    Create Animal.java

---
+++ Conditions-BeeCrowd/Beecrowd/Animal.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		String A1, A2, A3;
+   	 
+  	  Scanner input =new Scanner(System.in);
+  	  A1 = input.next();
+  	  A2 = input.next();
+  	  A3 = input.next();
+
+  	  if (A1.equals("vertebrado")  && A2.equals("ave") && A3.equals("carnivoro")) {
+  	   System.out.print("aguia\n");
+  	  }
+  	  if (A1.equals("vertebrado") && A2.equals("ave") && A3.equals("onivoro")) {
+  	   System.out.print("pomba\n");
+  	  }
+  	  if (A1.equals("vertebrado") && A2.equals("mamifero") && A3.equals("onivoro")) {
+  	   System.out.print("homem\n");
+  	  }
+  	  if (A1.equals("vertebrado") && A2.equals("mamifero") && A3.equals("herbivoro")) {
+  	   System.out.print("vaca\n");
+  	  }
+  	  
+  	  if (A1.equals("invertebrado") && A2.equals("inseto") && A3.equals("hematofago")) {
+  	   System.out.print("pulga\n");
+  	  }
+  	  if (A1.equals("invertebrado") && A2.equals("inseto") && A3.equals("herbivoro")) {
+  	   System.out.print("lagarta\n");
+  	  }
+  	  if (A1.equals("invertebrado") && A2.equals("anelideo") && A3.equals("hematofago")) {
+  	   System.out.print("sanguessuga\n");
+  	  }
+  	  if (A1.equals("invertebrado") && A2.equals("anelideo") && A3.equals("onivoro")) {
+  	   System.out.print("minhoca\n");
+  	  }
+
+  	    }
+
+  	}
