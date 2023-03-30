0b4d8b9be672839ec5fa9b25372c1fad1883234d
Kurt Cantuba <116888719+Sneakybooger@users.noreply.github.com>
2023-03-30 19:06:11 +0800

    Update and rename Conditions | Beecrowd answers/Grade Calculator.java to Conditions-BeeCrowd/Grade Calculator.java

--- Conditions-BeeCrowd/Grade Calculator.java
+++ Conditions-BeeCrowd/Grade Calculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ConditionalStatements {
+public class GradeCalculator {
 
 	public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
