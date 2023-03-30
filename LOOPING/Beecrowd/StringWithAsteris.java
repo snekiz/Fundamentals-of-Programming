682051e419a45443a136608ecdb8c938f315646a
Kurt Cantuba <116888719+Sneakybooger@users.noreply.github.com>
2023-03-30 18:59:02 +0800

    Update StringWithAsteris.java

--- LOOPING/Beecrowd/StringWithAsteris.java
+++ LOOPING/Beecrowd/StringWithAsteris.java
@@ -4,10 +4,13 @@ public class StringWithAsteris {
 	public static void main(String[] args) {
 //      Write a program that will read a string and print the string with * after each character
      Scanner sc = new Scanner(System.in);
-     
+     System.out.print("Enter a string: ");
      String s = sc.nextLine();
-     for(int i = s.length(); i>=0; i++) {
-    	   System.out.println(i);
+   
+     
+     for(int i = 0; i<s.length(); i++) {
+    	  char a = s.charAt(i);   	  
+    	   System.out.print(a + " *");
      }
 
 	}
